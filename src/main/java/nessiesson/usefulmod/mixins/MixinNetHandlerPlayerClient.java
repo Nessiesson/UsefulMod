8b54d09c9f4ab36bb8e5bfd7a6a909a7d888a101
nessie <nessie@hotmail.dk>
2018-03-25 21:08:02 +0200

    Print coordinates on death.

---
+++ src/main/java/nessiesson/usefulmod/mixins/MixinNetHandlerPlayerClient.java
@@ -0,0 +1,24 @@
+package nessiesson.usefulmod.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.play.server.SPacketCombatEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(NetHandlerPlayClient.class)
+public abstract class MixinNetHandlerPlayerClient {
+	@Inject(method = "handleCombatEvent", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/client/Minecraft;displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V"),
+			locals = LocalCapture.CAPTURE_FAILHARD)
+	private void onPlayerDeath(SPacketCombatEvent packetIn, CallbackInfo ci, Entity entity) {
+		BlockPos pos = entity.getPosition();
+		Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("You died @ " + pos.getX() + " " + pos.getY() + " " + pos.getZ()));
+	}
+}
