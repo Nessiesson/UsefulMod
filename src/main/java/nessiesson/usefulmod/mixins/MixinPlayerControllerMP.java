6798d05a38f33888cdde5b90a634366a5c442f04
nessie <nessie@hotmail.dk>
2019-09-17 21:26:25 +0200

    Add somewhat brittle workaround for reauth 1.12.2->1.12.0 server breaking craftinghax.

--- src/main/java/nessiesson/usefulmod/mixins/MixinPlayerControllerMP.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinPlayerControllerMP.java
@@ -1,13 +1,10 @@
 package nessiesson.usefulmod.mixins;
 
 import nessiesson.usefulmod.LiteModUsefulMod;
-import nessiesson.usefulmod.MixinCode;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -17,10 +14,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerControllerMP.class)
+@Mixin(value = PlayerControllerMP.class)
 public abstract class MixinPlayerControllerMP {
 	@Inject(method = "clickBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/PlayerControllerMP;onPlayerDestroyBlock(Lnet/minecraft/util/math/BlockPos;)Z", shift = At.Shift.AFTER))
 	private void onInstantMine(BlockPos loc, EnumFacing face, CallbackInfoReturnable<Boolean> cir) {
@@ -43,13 +39,8 @@ public abstract class MixinPlayerControllerMP {
 	private void onGetBlockReachDistance(CallbackInfoReturnable<Float> cir) {
 		if (LiteModUsefulMod.config.extendedExtendedReachHax) {
 			cir.setReturnValue(8F);
-		} else if(LiteModUsefulMod.config.extendedReachHax) {
+		} else if (LiteModUsefulMod.config.extendedReachHax) {
 			cir.setReturnValue(5F);
 		}
-	}
-
-	@Inject(method = "func_194338_a", at = @At("RETURN"))
-	private void craftingHax(int window, IRecipe recipe, boolean makeAll, EntityPlayer player, CallbackInfo ci) {
-		MixinCode.handleHaxCrafting(window);
 	}
 }
