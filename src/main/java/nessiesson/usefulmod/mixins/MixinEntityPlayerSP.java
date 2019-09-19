c765f7357d06ea38b456e191c13efdc77ebf901c
nessie <nessie@hotmail.dk>
2019-09-19 22:33:46 +0200

    Prevent villager gui from dropping items when closing (if possible).

--- src/main/java/nessiesson/usefulmod/mixins/MixinEntityPlayerSP.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinEntityPlayerSP.java
@@ -2,12 +2,17 @@ package nessiesson.usefulmod.mixins;
 
 import com.mojang.authlib.GameProfile;
 import nessiesson.usefulmod.LiteModUsefulMod;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.inventory.ClickType;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerMerchant;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityPlayerSP.class)
@@ -20,6 +25,16 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
 	private void overrideCommandPermissions(int permLevel, String commandName, CallbackInfoReturnable<Boolean> cir) {
 		if (LiteModUsefulMod.config.alwaysSingleplayerCheats) {
 			cir.setReturnValue(true);
+		}
+	}
+
+	@Inject(method = "closeScreen", at = @At("HEAD"))
+	private void onCloseScreen(CallbackInfo ci) {
+		final Container container = this.openContainer;
+		if (container instanceof ContainerMerchant) {
+			final Minecraft mc = Minecraft.getMinecraft();
+			mc.playerController.windowClick(container.windowId, 0, 1, ClickType.QUICK_MOVE, mc.player);
+			mc.playerController.windowClick(container.windowId, 1, 1, ClickType.QUICK_MOVE, mc.player);
 		}
 	}
 }
