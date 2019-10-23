073ca15e35ca9af039872f3b57343d1bbd7b8a00
nessie <nessie@hotmail.dk>
2019-10-23 18:11:54 +0200

    Add hackerman only attack nondead mobs stolen directly from tweakeroo.

---
+++ src/main/java/nessiesson/usefulmod/mixins/MixinEntityRenderer$Predicate.java
@@ -0,0 +1,20 @@
+package nessiesson.usefulmod.mixins;
+
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+// Totally not stolen from hackerman masa
+// https://github.com/maruohon/tweakeroo/blob/521e641e34c709eaeb683176f89c9952e5c355f9/src/main/java/fi/dy/masa/tweakeroo/mixin/MixinEntityRenderer_Predicate.java
+@Mixin(targets = "net/minecraft/client/renderer/EntityRenderer$1")
+public abstract class MixinEntityRenderer$Predicate {
+	@Inject(method = "apply", at = @At("HEAD"), cancellable = true)
+	private void ignoreDeadEntities(@Coerce Object entity, CallbackInfoReturnable<Boolean> cir) {
+		if (entity instanceof EntityLivingBase && ((EntityLivingBase) entity).getHealth() <= 0F) {
+			cir.setReturnValue(false);
+		}
+	}
+}
