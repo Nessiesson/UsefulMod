eb7c603281a9a01e33557915b5b34b0a99cce886
nessie <nessie@hotmail.dk>
2019-12-05 04:06:55 +0100

    Fix ghost block fix not working. The cause was checking for the block hardness "too late".

--- src/main/java/nessiesson/usefulmod/mixins/MixinPlayerControllerMP.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinPlayerControllerMP.java
@@ -1,7 +1,6 @@
 package nessiesson.usefulmod.mixins;
 
 import nessiesson.usefulmod.LiteModUsefulMod;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.client.network.NetHandlerPlayClient;
@@ -9,6 +8,7 @@ import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,15 +16,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerControllerMP.class)
 public abstract class MixinPlayerControllerMP {
-	@Inject(method = "clickBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/PlayerControllerMP;onPlayerDestroyBlock(Lnet/minecraft/util/math/BlockPos;)Z", shift = At.Shift.AFTER))
-	private void onInstantMine(BlockPos loc, EnumFacing face, CallbackInfoReturnable<Boolean> cir) {
-		if (LiteModUsefulMod.config.miningGhostBlockFix) {
-			final Minecraft mc = Minecraft.getMinecraft();
-			final IBlockState block = mc.world.getBlockState(loc);
-			if (block.getBlockHardness(mc.world, loc) > 0.0f) {
-				final NetHandlerPlayClient connection = mc.getConnection();
-				connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(loc, face, EnumHand.MAIN_HAND, 0f, 0f, 0f));
-			}
+	final private static String onInstantMine = "Lnet/minecraft/client/multiplayer/PlayerControllerMP;onPlayerDestroyBlock(Lnet/minecraft/util/math/BlockPos;)Z";
+	float myHardness;
+
+	@Inject(method = "clickBlock", at = @At(value = "INVOKE", target = onInstantMine, shift = At.Shift.BEFORE))
+	private void preInstantMine(BlockPos pos, EnumFacing face, CallbackInfoReturnable<Boolean> cir) {
+		final World world = Minecraft.getMinecraft().world;
+		this.myHardness = world.getBlockState(pos).getBlockHardness(world, pos);
+	}
+
+	@Inject(method = "clickBlock", at = @At(value = "INVOKE", target = onInstantMine, shift = At.Shift.AFTER))
+	private void postInstantMine(BlockPos pos, EnumFacing face, CallbackInfoReturnable<Boolean> cir) {
+		if (LiteModUsefulMod.config.miningGhostBlockFix && this.myHardness > 0F) {
+			final NetHandlerPlayClient connection = Minecraft.getMinecraft().getConnection();
+			connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(pos, face, EnumHand.MAIN_HAND, 0F, 0F, 0F));
 		}
 	}
 }
