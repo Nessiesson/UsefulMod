deb0ae3199f6dcf5271e083f3828ae399f34d32e
nessie <nessie@hotmail.dk>
2020-01-17 02:52:57 +0100

    fix incorrect logic in noattributes.

--- src/main/java/nessiesson/usefulmod/mixins/MixinItemStack.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinItemStack.java
@@ -18,7 +18,7 @@ public abstract class MixinItemStack {
 
 	@Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Multimap;isEmpty()Z", remap = false))
 	private boolean noAttributes(Multimap multimap) {
-		return LiteModUsefulMod.config.showItemAttributes || multimap.isEmpty() || !Minecraft.getMinecraft().gameSettings.advancedItemTooltips;
+		return !LiteModUsefulMod.config.showItemAttributes || multimap.isEmpty() || !Minecraft.getMinecraft().gameSettings.advancedItemTooltips;
 	}
 
 	@Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getEnchantmentTagList()Lnet/minecraft/nbt/NBTTagList;"))
