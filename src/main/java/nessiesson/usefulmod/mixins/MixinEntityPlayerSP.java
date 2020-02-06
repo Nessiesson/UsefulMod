c622532872dc554183652c2ea41f1fa6b26452ed
nessie <nessie@hotmail.dk>
2020-02-06 01:30:11 +0100

    apply epic fix for MC-2071.

--- src/main/java/nessiesson/usefulmod/mixins/MixinEntityPlayerSP.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinEntityPlayerSP.java
@@ -5,6 +5,7 @@ import nessiesson.usefulmod.LiteModUsefulMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerMerchant;
@@ -12,6 +13,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -36,5 +38,10 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
 			mc.playerController.windowClick(container.windowId, 0, 1, ClickType.QUICK_MOVE, mc.player);
 			mc.playerController.windowClick(container.windowId, 1, 1, ClickType.QUICK_MOVE, mc.player);
 		}
+	}
+
+	@Redirect(method = "onLivingUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiScreen;doesGuiPauseGame()Z"))
+	private boolean mc2071(GuiScreen gui) {
+		return true;
 	}
 }
