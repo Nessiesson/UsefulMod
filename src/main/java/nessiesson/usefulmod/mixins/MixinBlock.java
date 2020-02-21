0e454643dde7b930f268492c861110227d0a0d96
nessie <nessie@hotmail.dk>
2020-02-21 18:08:03 +0100

    add fix for mc-2399

--- src/main/java/nessiesson/usefulmod/mixins/MixinBlock.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
@@ -18,5 +19,17 @@ public abstract class MixinBlock {
 		if (LiteModUsefulMod.config.centeredPlants) {
 			cir.setReturnValue(Vec3d.ZERO);
 		}
+	}
+
+	// fix for MC-2399 by MrGrim https://github.com/mrgrim/MUP/commit/45ddb6fbb93c6f6f3d89ba05de9a6345e108d307
+	@ModifyVariable(method = "registerBlocks", name = "flag4", at = @At(value = "STORE", ordinal = 0))
+	private static boolean noTranslucentNeighborBrightness(boolean flag4) {
+		return false;
+	}
+
+	// fix for MC-2399 by MrGrim https://github.com/mrgrim/MUP/commit/45ddb6fbb93c6f6f3d89ba05de9a6345e108d307
+	@ModifyVariable(method = "registerBlocks", name = "flag5", at = @At(value = "STORE", ordinal = 0))
+	private static boolean noZeroOpacityNeighborBrightness(boolean flag5) {
+		return false;
 	}
 }
