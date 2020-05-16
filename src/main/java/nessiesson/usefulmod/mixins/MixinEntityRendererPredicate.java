3b15adca50acf35d518f948bbec581354ad05d78
nessie <nessie@hotmail.dk>
2020-05-16 19:28:45 +0200

    Rename class to prevent IDE errors.

--- src/main/java/nessiesson/usefulmod/mixins/MixinEntityRendererPredicate.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinEntityRendererPredicate.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 // Totally not stolen from hackerman masa
 // https://github.com/maruohon/tweakeroo/blob/521e641e34c709eaeb683176f89c9952e5c355f9/src/main/java/fi/dy/masa/tweakeroo/mixin/MixinEntityRenderer_Predicate.java
 @Mixin(targets = "net/minecraft/client/renderer/EntityRenderer$1")
-public abstract class MixinEntityRenderer$Predicate {
+public abstract class MixinEntityRendererPredicate {
 	@Inject(method = "apply(Lnet/minecraft/entity/Entity;)Z", at = @At("HEAD"), cancellable = true)
 	private void ignoreDeadEntities(Entity entity, CallbackInfoReturnable<Boolean> cir) {
 		if (entity instanceof EntityLivingBase && ((EntityLivingBase) entity).getHealth() <= 0F) {
