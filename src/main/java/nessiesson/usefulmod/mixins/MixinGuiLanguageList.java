2df28764406d38787db0dda0b11049fbda7bb71c
nessie <nessie@hotmail.dk>
2020-05-29 03:17:57 +0200

    Fix fast language switch not working.

--- src/main/java/nessiesson/usefulmod/mixins/MixinGuiLanguageList.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinGuiLanguageList.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "net/minecraft/client/gui/GuiLanguage$List")
 public abstract class MixinGuiLanguageList {
-	@Redirect(method = "elementClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;refreshResources()V"))
+	@Redirect(method = "elementClicked(IZII)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;refreshResources()V"))
 	private void onlyRefreshLanguage(Minecraft mc) {
 		mc.getLanguageManager().onResourceManagerReload(mc.getResourceManager());
 	}
