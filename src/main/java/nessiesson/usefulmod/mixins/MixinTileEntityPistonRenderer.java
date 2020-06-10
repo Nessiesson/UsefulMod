cad196be6b6d46319ec85257d51aa7e62f6829bc
nessie <nessie@hotmail.dk>
2020-06-10 20:50:37 +0200

    Try to prevent z-fighting when moving blocks through each other.

--- src/main/java/nessiesson/usefulmod/mixins/MixinTileEntityPistonRenderer.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinTileEntityPistonRenderer.java
@@ -14,13 +14,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TileEntityPistonRenderer.class)
 public abstract class MixinTileEntityPistonRenderer {
 	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;disableCull()V"))
-	private void cullMovingBlocks() {
+	private void cullMovingBlocks(TileEntityPiston te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 		GlStateManager.enableCull();
+		final double f = 0.9997 + 0.0001D * te.getFacing().getIndex();
+		GlStateManager.scale(f, f, f);
 	}
 
 	@Inject(method = "render", at = @At("RETURN"))
 	private void uncullMovingBlocks(TileEntityPiston te, double x, double y, double z, float partialTicks, int destroyStage, float alpha, CallbackInfo ci) {
 		GlStateManager.disableCull();
+		GlStateManager.scale(1D, 1D, 1D);
 	}
 
 	@ModifyConstant(method = "render", constant = @Constant(floatValue = 0.25F))
